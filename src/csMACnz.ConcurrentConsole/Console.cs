a5d66e280bc9bdf279e25dcccefea927af2e0a38
Mark Clearwater <csmacnz@csmac.co.nz>
2016-08-09 19:59:37 +0100

    Added test harness and initial code

--- src/csMACnz.ConcurrentConsole/Console.cs
+++ src/csMACnz.ConcurrentConsole/Console.cs
@@ -4,9 +4,35 @@ namespace csMACnz.ConcurrentConsole
 {
     public class Console
     {
+        public string Prompt { get; set; }
+        private string _input = null;
         public string ReadLine()
-        {    
-		    return string.Empty;
+        {
+            _input = "";
+            while(true){
+                var value = System.Console.ReadKey();
+                if((value.Modifiers & ConsoleModifiers.Alt) != 0){}
+                else if((value.Modifiers & ConsoleModifiers.Shift) != 0){}
+                else if((value.Modifiers & ConsoleModifiers.Control) != 0){}
+                if(value.Key == ConsoleKey.Enter){
+                    break; 
+                }
+                else if(value.Key == ConsoleKey.Escape){
+                    _input = "";
+                }
+                else
+                {
+                    _input += value.KeyChar;
+                }
+            }
+            var result = _input;
+            _input = null;
+            return result;
+        }
+
+        public void WriteLine(string value)
+        {
+            System.Console.WriteLine(value);
         }
     }
 }
